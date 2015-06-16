7f1878eb4a480c45cb9489651eac33b112eb68ae
Ismail Marmoush <marmoushismail@gmail.com>
2015-06-16 02:18:17 +0200

    Editing Configuration files

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -29,8 +29,8 @@ console.log('\'Ello \'Ello!');
 /********* Objects *********/
 // Selectors take normal name
 // Classes take trailing 'Class', e.g gradientClass
-var configContent={};
-var configApp={};
+var configContent = {};
+var configApp = {};
 var Backend = {};
 var Utils = {};
 var Animation = {};
@@ -40,141 +40,141 @@ var GithubApi = {};
 /******************** Utilities **************************/
 
 Utils.getHash = function() {
-    'use strict';
-    var url = window.location.hash;
-    var hash = url.substring(url.indexOf('#') + 1);
-    return hash;
+  'use strict';
+  var url = window.location.hash;
+  var hash = url.substring(url.indexOf('#') + 1);
+  return hash;
 };
 Utils.isGithubApiLink = function(url) {
-    'use strict';
-    if (Utils.startsWith(url, 'https://api.github.com/repos')) {
-        return true;
-    } else {
-        return false;
-    }
+  'use strict';
+  if (Utils.startsWith(url, 'https://api.github.com/repos')) {
+    return true;
+  } else {
+    return false;
+  }
 };
 // TODO
 Utils.selectMenu = function() {
-    'use strict';
-    //$(configApp.lsb + ' li').removeClass('selected');
-    //$(configApp.lsb + ' a[href^="#' + val.url + '"]').parent().addClass('selected');
+  'use strict';
+  //$(configApp.lsbId + ' li').removeClass('selected');
+  //$(configApp.lsbId + ' a[href^="#' + val.url + '"]').parent().addClass('selected');
 };
 
 Utils.fitSize = function(src, dest) {
-    'use strict';
-    $(dest).height($(src).height());
-    $(dest).width($(src).width());
+  'use strict';
+  $(dest).height($(src).height());
+  $(dest).width($(src).width());
 };
 
 Utils.fitGradientToImg = function() {
-    'use strict';
-    console.log('Fitting gradient size to header img size');
-    Utils.fitSize(configApp.headerImg, configApp.gradientContainer);
+  'use strict';
+  console.log('Fitting gradient size to header img size');
+  Utils.fitSize(configApp.headerImgId, configApp.gradientContainerId);
 };
 
 Utils.gradientInit = function() {
-    'use strict';
-    $.each([configApp.headerImg, window], function(k, v) {
-        $(v).resize(function() {
-            Utils.fitGradientToImg();
-        });
+  'use strict';
+  $.each([configApp.headerImgId, window], function(k, v) {
+    $(v).resize(function() {
+      Utils.fitGradientToImg();
     });
+  });
 };
 
 Utils.showErrorMsg = function(msg) {
-    'use strict';
-    // TODO Add some UX
-    $(configApp.errorId).html();
+  'use strict';
+  // TODO Add some UX
+  $(configApp.errorId).html();
 };
 
 Utils.startsWith = function(str, val) {
-    'use strict';
-    if (str !== undefined && str.indexOf(val, 0) === 0) {
-        return true;
-    } else {
-        return false;
-    }
+  'use strict';
+  if (str !== undefined && str.indexOf(val, 0) === 0) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 /******************** App Animation **************************/
 
 
 Animation.smoothScrolling = function() {
-    'use strict';
-    $('#rsb a[href^="#"]').on('click', function(e) {
-        e.preventDefault();
-        var target = this.hash;
-        var $target = $(target);
-        $('html, body').stop().animate({
-            'scrollTop': $target.offset().top - 50
-        }, 900, 'swing', function() {
-            // commenting this will prevent the url to be changed
-            //window.location.hash = target;
-        });
+  'use strict';
+  $('#rsb a[href^="#"]').on('click', function(e) {
+    e.preventDefault();
+    var target = this.hash;
+    var $target = $(target);
+    $('html, body').stop().animate({
+      'scrollTop': $target.offset().top - 50
+    }, 900, 'swing', function() {
+      // commenting this will prevent the url to be changed
+      //window.location.hash = target;
     });
+  });
 };
 
 Animation.scrollTo = function(element) {
-    'use strict';
-    $('html, body').animate({
-        scrollTop: $(element).offset().top - 200
-    }, 900);
+  'use strict';
+  $('html, body').animate({
+    scrollTop: $(element).offset().top - 200
+  }, 900);
 };
 
 Animation.toggleLeftSidebar = function(selectors) {
-    'use strict';
-    var bc = 'moveContentRight';
-    var lsb = 'moveLSB';
+  'use strict';
+  var bc = 'moveContentRight';
+  var lsb = 'moveLSB';
 
-    $.each(selectors, function(key, val) {
-        $(val).click(function() {
-            $(configApp.bar).toggleClass(bc);
-            $(configApp.content).toggleClass(bc);
-            $(configApp.lsb).toggleClass(lsb);
-            if ($(configApp.content).hasClass(bc)) {
-                $(configApp.content).one('click', function() {
-                    $(configApp.bar).removeClass(bc);
-                    $(configApp.content).removeClass(bc);
-                    $(configApp.lsb).removeClass(lsb);
-                });
-            }
+  $.each(selectors, function(key, val) {
+    $(val).click(function() {
+      $(configApp.barId).toggleClass(bc);
+      $(configApp.contentId).toggleClass(bc);
+      $(configApp.lsbId).toggleClass(lsb);
+      if ($(configApp.contentId).hasClass(bc)) {
+        $(configApp.contentId).one('click', function() {
+          $(configApp.barId).removeClass(bc);
+          $(configApp.contentId).removeClass(bc);
+          $(configApp.lsbId).removeClass(lsb);
         });
+      }
     });
+  });
 };
 
 Animation.toggleRightSidebar = function(selectors) {
-    'use strict';
-    var bc = 'moveContentLeft';
-    var rsb = 'moveRSB';
-    $.each(selectors, function(key, val) {
-        $(val).click(function() {
-            $(configApp.bar).toggleClass(bc);
-            $(configApp.rsb).toggleClass(rsb);
-            if ($(configApp.rsb).hasClass(rsb)) {
-                $(configApp.content).one('click', function() {
-                    $(configApp.bar).removeClass(bc);
-                    $(configApp.rsb).removeClass(rsb);
-                });
-            }
+  'use strict';
+  var bc = 'moveContentLeft';
+  var rsb = 'moveRSB';
+  $.each(selectors, function(key, val) {
+    $(val).click(function() {
+      $(configApp.barId).toggleClass(bc);
+      $(configApp.rsbId).toggleClass(rsb);
+      if ($(configApp.rsbId).hasClass(rsb)) {
+        $(configApp.contentId).one('click', function() {
+          $(configApp.barId).removeClass(bc);
+          $(configApp.rsbId).removeClass(rsb);
         });
+      }
     });
+  });
 };
 /*************** END App Animation ************/
 
 /*************** Comment System ************/
 
 Comments.disqusReload = function(enableDisqus, disqusIdentifier, language, title) {
-    'use strict';
-    if (language === undefined) {
-        language = 'en';
-    }
+  'use strict';
+  if (language === undefined) {
+    language = 'en';
+  }
 
-    if (enableDisqus) {
-        $(configApp.disqusThread).show();
-        disqusReset(disqusIdentifier, location.origin + disqusIdentifier, title, language);
-    } else {
-        $(configApp.disqusThread).hide();
-    }
+  if (enableDisqus) {
+    $(configApp.disqusThreadId).show();
+    disqusReset(disqusIdentifier, location.origin + disqusIdentifier, title, language);
+  } else {
+    $(configApp.disqusThreadId).hide();
+  }
 };
 
 /************** End Comment System ***********/
@@ -183,206 +183,207 @@ Comments.disqusReload = function(enableDisqus, disqusIdentifier, language, title
 
 /******************** Content Manipulation **************************/
 Content.markdown = function(md, callback) {
-    'use strict';
-    var options = {
-        highlight: function(code) {
-            return hljs.highlightAuto(code).value;
-        }
-    };
-    marked(md, options, function(err, output) {
-        if (err) {
-            throw err;
-        }
-        callback(output);
-    });
+  'use strict';
+  var options = {
+    highlight: function(code) {
+      return hljs.highlightAuto(code).value;
+    }
+  };
+  marked(md, options, function(err, output) {
+    if (err) {
+      throw err;
+    }
+    callback(output);
+  });
 };
 
 Content.runToc = function(container) {
-    'use strict';
-    container = typeof container !== 'undefined' ? container : '#post';
-    $(configApp.rsb).toc({
-        'selectors': 'h1,h2,h3,h4,h5,h6',
-        'container': container,
-        'smoothScrolling': false,
-        'prefix': 'toc',
-        'onHighlight': function(el) {},
-        'highlightOnScroll': true,
-        'highlightOffset': 100,
-        'anchorName': function(i, heading, prefix) {
-            return prefix + i;
-        },
-        'headerText': function(i, heading, $heading) {
-            return $heading.text();
-        }
-    });
+  'use strict';
+  container = typeof container !== 'undefined' ? container : '#post';
+  $(configApp.lsbId).toc({
+    'selectors': 'h1,h2,h3,h4,h5,h6',
+    'container': container,
+    'smoothScrolling': false,
+    'prefix': 'toc',
+    'onHighlight': function(el) {},
+    'highlightOnScroll': true,
+    'highlightOffset': 100,
+    'anchorName': function(i, heading, prefix) {
+      return prefix + i;
+    },
+    'headerText': function(i, heading, $heading) {
+      return $heading.text();
+    }
+  });
 };
 
 Content.fillSlider = function(data) {
-    'use strict';
-    var blog = data.blog;
-    $.each(blog, function(key, val) {
-        // fill slides
-        console.log(val.url);
-        var img = '<img src="' + val.thumb + '" />';
-        var p = '<p>' + val.header.title + '</p>';
-        var p2 = '<p>' + val.date + '</p>';
-        var li = '<li><a href="#' + val.url + '">' + img + p + p2 + '</a></li>';
-        $(li).appendTo(configApp.slides);
-    });
+  'use strict';
+  var blog = data.blog;
+  $.each(blog, function(key, val) {
+    // fill slides
+    console.log(val.url);
+    var img = '<img src="' + val.thumb + '" />';
+    var p = '<p>' + val.header.title + '</p>';
+    var p2 = '<p>' + val.date + '</p>';
+    var li = '<li><a href="#' + val.url + '">' + img + p + p2 + '</a></li>';
+    $(li).appendTo(configApp.slidesId);
+  });
 };
 
 Content.updateBrowserTitle = function(title) {
-    'use strict';
-    document.title = title;
+  'use strict';
+  document.title = title;
 };
 
 Content.setHeaderImg = function(imgSrc, addGradient, title, subtitle) {
-    'use strict';
-    $(configApp.headerImg).attr('src', imgSrc);
-    Utils.fitGradientToImg();
-    if (configApp.headerImg && addGradient) {
-        $(configApp.gradientContainer).addClass(configApp.gradientClass);
-    } else {
-        $(configApp.gradientContainer).removeClass(configApp.gradientClass);
-    }
-    // Update Header Title
-    if (title) {
-        title = '';
-    }
-    if (subtitle) {
-        subtitle = '';
-    }
-    //    $('#title').text(title);
-    //    $('#subtitle').text(subtitle);
+  'use strict';
+  $(configApp.headerImgId).attr('src', imgSrc);
+  Utils.fitGradientToImg();
+  if (configApp.headerImgId && addGradient) {
+    $(configApp.gradientContainerId).addClass(configApp.gradientClass);
+  } else {
+    $(configApp.gradientContainerId).removeClass(configApp.gradientClass);
+  }
+  // Update Header Title
+  if (title) {
+    title = '';
+  }
+  if (subtitle) {
+    subtitle = '';
+  }
+  //    $('#title').text(title);
+  //    $('#subtitle').text(subtitle);
 };
 
 
 Content.reloadPage = function(val) {
-    'use strict';
-    $(configApp.rsb).show();
-    $(configApp.contentIcon).show();
-    Content.updateBrowserTitle(configContent.title + val.header.title);
-    Backend.loadContent(val.url, function(data) {
-        Content.setHeaderImg(val.header.img, val.header.addGradient, val.header.title, val.header.subtitle);
-        Content.markdown(data, function(compiledMarkdown) {
-            $(configApp.post).html(compiledMarkdown);
-            $('pre').addClass('hljs');
-        });
-        Content.runToc();
-        Animation.smoothScrolling();
-        Comments.disqusReload(val.disqus.enable, val.disqus.identifier, val.disqus.lang, document.title);
+  'use strict';
+  $(configApp.rsb).show();
+  $(configApp.contentIconId).show();
+  Content.updateBrowserTitle(configContent.title + val.header.title);
+  Backend.loadContent(val.url, function(data) {
+    Content.setHeaderImg(val.header.img, val.header.addGradient, val.header.title, val.header.subtitle);
+    Content.markdown(data, function(compiledMarkdown) {
+      $(configApp.postId).html(compiledMarkdown);
+      $('pre').addClass('hljs');
     });
+    Content.runToc();
+    Animation.smoothScrolling();
+    Comments.disqusReload(val.disqus.enable, val.disqus.identifier, val.disqus.lang, document.title);
+  });
 
 };
 
-Content.routes = function(data) {
-    'use strict';
-    var gh = data.global.header;
-    var blog = data.blog;
-    var pages = data.pages;
-    var urls = {};
-    $.each(blog, function(key, val) {
-        // Routing listener
-        urls[val.url] = function() {
-            $(configApp.post).html('');
-            $(configApp.slides).fadeOut(500, function() {
-                $(configApp.blog).fadeIn(500, function() {
-                    Content.reloadPage(val);
-                });
-            });
-        };
-    });
+Content.routes = function() {
+  'use strict';
 
-    $.each(pages, function(key, val) {
-        urls[val.url] = function() {
-            console.log('Showing page:' + val.url);
-            $(configApp.post).html('');
-            $(configApp.slides).fadeOut(500, function() {
-                $(configApp.blog).fadeIn(500, function() {
-                    Content.reloadPage(val);
-                });
-            });
-        };
-    });
-
-    urls[''] = function() {
-        $(configApp.post).html('');
-        $(configApp.rsb).hide();
-        $(configApp.contentIcon).hide();
-        Content.updateBrowserTitle(configApp.title);
-        Content.setHeaderImg(gh.img, gh.addGradient, gh.title, gh.subtitle);
-        $(configApp.blog).fadeOut(500, function() {
-            $(configApp.slides).fadeIn(500, function() {
-                Utils.fitGradientToImg();
-            });
+  var urls = {};
+  $.each(configContent.blog, function(key, val) {
+    // Routing listener
+    urls[val.url] = function() {
+      $(configApp.postId).html('');
+      $(configApp.slidesId).fadeOut(500, function() {
+        $(configApp.blogId).fadeIn(500, function() {
+          Content.reloadPage(val);
         });
+      });
     };
-    routie(urls);
+  });
+
+  $.each(configContent.pages, function(key, val) {
+    urls[val.url] = function() {
+      console.log('Showing page:' + val.url);
+      $(configApp.postId).html('');
+      $(configApp.slidesId).fadeOut(500, function() {
+        $(configApp.blogId).fadeIn(500, function() {
+          Content.reloadPage(val);
+        });
+      });
+    };
+  });
+
+  urls[''] = function() {
+    $(configApp.postId).html('');
+    $(configApp.rsbId).hide();
+    $(configApp.contentIconIdId).hide();
+    Content.updateBrowserTitle(configApp.title);
+    Content.setHeaderImg(configContent.global.header.img, configContent.global.header.addGradient, configContent.global.header.title, configContent.global.header.subtitle);
+    $(configApp.blogId).fadeOut(500, function() {
+      $(configApp.slidesId).fadeIn(500, function() {
+        Utils.fitGradientToImg();
+      });
+    });
+  };
+  routie(urls);
 };
 /******************* Github API ****************/
 GithubApi.loadReadme = function(path, callback) {
-    'use strict';
-    $.ajax({
-        url: path,
-        dataType: 'json',
-        type: 'GET',
-        async: true,
-        statusCode: {
-            404: function(response) {
+  'use strict';
+  $.ajax({
+    url: path,
+    dataType: 'json',
+    type: 'GET',
+    async: true,
+    statusCode: {
+      404: function(response) {
 
-            },
-            200: function(response) {
-                // Decoding base64
-                var d = window.atob(response.content);
-                callback(d);
-            }
-        },
-        error: function(jqXHR, status, errorThrown) {
-            Utils.showErrorMsg(jqXHR + ':' + status + ':' + ':' + errorThrown);
-        }
-    });
+      },
+      200: function(response) {
+        // Decoding base64
+        var d = window.atob(response.content);
+        callback(d);
+      }
+    },
+    error: function(jqXHR, status, errorThrown) {
+      Utils.showErrorMsg(jqXHR + ':' + status + ':' + ':' + errorThrown);
+    }
+  });
 };
 
 /******************* Backend ********************/
 
 Backend.loadJson = function(path, callback) {
-    'use strict';
-    $.getJSON(path, function(data) {
-        callback(data);
-    });
+  'use strict';
+  $.getJSON(path, function(data) {
+    callback(data);
+  });
 };
 
 Backend.loadContent = function(url, callback) {
-    'use strict';
-    if (Utils.isGithubApiLink(url)) {
-        GithubApi.loadReadme(url, function(data) {
-            callback(data);
-        });
-    } else {
-        $('<div></div>').load(url, function(data, status, xhr) {
-            if (status === 'error') {
-                var msg = 'Sorry but there was an error: ';
-                Utils.showErrorMsg(msg + xhr.status + ' ' + xhr.statusText);
-            }
-            if (status === 'success') {
-                callback(data);
-            }
-        });
-    }
+  'use strict';
+  if (Utils.isGithubApiLink(url)) {
+    GithubApi.loadReadme(url, function(data) {
+      callback(data);
+    });
+  } else {
+    $('<div></div>').load(url, function(data, status, xhr) {
+      if (status === 'error') {
+        var msg = 'Sorry but there was an error: ';
+        Utils.showErrorMsg(msg + xhr.status + ' ' + xhr.statusText);
+      }
+      if (status === 'success') {
+        callback(data);
+      }
+    });
+  }
 };
 
 //function sideBarOpened
 $(document).ready(function() {
-    'use strict';
-    console.log('loading routes, filling sliders');
+  'use strict';
+  console.log('loading routes, filling sliders');
 
-    Backend.loadJson('config-content.json', function(data) {
-        Content.routes(data);
-        Content.fillSlider(data);
-    });
-
-    Animation.toggleLeftSidebar([configApp.menuIcon, configApp.lsb]);
-    Animation.toggleRightSidebar([configApp.contentIcon, configApp.rsb]);
+  Backend.loadJson('config-app.json', function(data) {
+    configApp = data;
+    Animation.toggleLeftSidebar([configApp.menuIconId, configApp.lsbId]);
+    Animation.toggleRightSidebar([configApp.contentIconId, configApp.rsbId]);
     Animation.smoothScrolling();
     Utils.gradientInit();
+    Backend.loadJson('config-content.json', function(data) {
+      configContent = data;
+      Content.routes();
+      Content.fillSlider(data);
+    });
+  });
 });
